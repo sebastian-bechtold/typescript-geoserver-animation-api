499539a6bf87d5da38b83a06852668684b2b1db3
Sebastian Bechtold <sbecht@geomer.de>
2017-10-11 15:37:47 +0200

    Added new Vue.js components

---
+++ src/FlowAnimationsList.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="flow-animations-list">        
+        <ul>
+            <li v-for="anim in animations" @click="onAnimationClicked(anim, $event)">{{anim.title}}</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { GeoServerAnimationApi } from './GeoServerAnimationApi'
+import { Component, Inject, Model, Prop, Vue, Watch } from 'vue-property-decorator'
+
+@Component({})
+export default class FlowAnimationsList extends Vue {
+
+    //################# BEGIN Props ###################
+    @Prop()
+    geoServerUrl: string;
+
+    @Prop()
+    proxyUrl: string;
+    //################# END Props ###################
+
+    private animations: any = [];
+
+    created() {
+        let gsanim = new GeoServerAnimationApi(this.geoServerUrl, this.proxyUrl);
+
+        let prefix = "flow_";
+        
+
+        gsanim.loadAnimationsFromAllWorkspaces(prefix, (result: any) => {
+
+            for (let layerGroup of result) {
+
+                gsanim.loadAnimationAsync(layerGroup.name, layerGroup.name, (animDetailInfo: any) => {
+                    this.animations.push(animDetailInfo);
+                });
+            }
+        });
+    }
+
+    onAnimationClicked(animation: any, event: MouseEvent) {
+        this.$emit('input', animation);
+    }
+
+}
+</script>
+
+<style lang="scss">
+div.flow-animations-list {
+
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+
+        >li {
+            border-bottom: 1px solid #ccc;
+            padding: 8px;
+
+            &:hover {
+                background-color: #eee;
+                cursor: pointer;
+            }
+        }
+    }
+}
+</style>
---
+++ src/RasterAnimationsList.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="raster-animations-list">
+        <ul>
+            <li v-for="anim in animations" @click="onAnimationClicked(anim, $event)">{{anim.title}}</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { GeoServerAnimationApi } from './GeoServerAnimationApi'
+import { Component, Inject, Model, Prop, Vue, Watch } from 'vue-property-decorator'
+
+@Component({})
+export default class RasterAnimationsList extends Vue {
+
+    //################# BEGIN Props ###################
+    @Prop()
+    geoServerUrl: string;
+
+    @Prop()
+    proxyUrl: string;
+    //################# END Props ###################
+
+    private animations: any = [];
+
+    created() {
+        var gsanim = new GeoServerAnimationApi(this.geoServerUrl, this.proxyUrl);
+
+        let prefix = "anim_";
+
+        gsanim.loadAnimationsFromAllWorkspaces(prefix, (result: any) => {
+
+            for (let lg of result) {
+
+                gsanim.loadAnimationAsync(lg.name, lg.name, (animDetailInfo: any) => {
+                    this.animations.push(animDetailInfo);
+                });
+            }
+        });
+    }
+
+    onAnimationClicked(animation: any, event: MouseEvent) {
+        this.$emit('input', animation);
+    }
+
+}
+</script>
+
+<style lang="scss">
+div.raster-animations-list {
+
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+
+        >li {
+            border-bottom: 1px solid #ccc;
+            padding: 8px;
+
+            &:hover {
+                background-color: #eee;
+                cursor: pointer;
+            }
+        }
+    }
+}
+</style>
