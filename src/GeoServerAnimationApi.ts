2abb33675c7f04f344d0437842d5abc8d968cd0b
Sebastian Bechtold <sbecht@geomer.de>
2017-10-11 15:35:46 +0200

    Small changes

--- src/GeoServerAnimationApi.ts
+++ src/GeoServerAnimationApi.ts
@@ -1,75 +1,76 @@
+// TODO: 2 Add support for request failure handlers
 // TODO: 3 Use promises
 
-import {GeoServerRestApi} from "./typescript-geoserver-rest-api/src/GeoServerRestApi";
+
+import { GeoServerRestApi } from "./typescript-geoserver-rest-api/src/GeoServerRestApi";
 
 export class GeoServerAnimationApi {
 
     gsRestApi: GeoServerRestApi;
-   
-    constructor(geoserverUrl: string, proxyUrl : string) {
+
+    constructor(geoserverUrl: string, proxyUrl: string) {
         this.gsRestApi = new GeoServerRestApi(geoserverUrl, proxyUrl);
+
     }
 
 
-    loadAnimationAsync(workspace : string, name : string, handler : any) {
-        
+    loadAnimationAsync(workspace: string, name: string, handler: any) {
+
         let me = this;
-        
-        this.gsRestApi.loadLayerGroupAsync(workspace, name, function(result : any) {                        
+
+        this.gsRestApi.loadLayerGroupAsync(workspace, name, function (result: any) {
 
             if (typeof result == "undefined") {
                 console.error("Failed to get animation " + workspace + ":" + name);
-                return;              
+                return;
             }
-            
+
             // Attach URL of GeoServer source:
             result.geoServerUrl = me.gsRestApi.geoServerBaseUrl;
-            
+
             handler(result);
         });
     }
 
 
     // getAnimations looks for animation group layers in the passed workspace
-    loadAllAnimationsFromWorkspaceAsync(workspace: string, handler : any) {
-        
-        this.gsRestApi.loadLayerGroupListAsync(workspace, function (layerGroups : any) {
-            
+    loadAllAnimationsFromWorkspaceAsync(workspace: string, prefix: string, handler: any) {
+
+        this.gsRestApi.loadLayerGroupListAsync(workspace, (layerGroups: any) => {
+
             let result = [];
 
             //###### BEGIN Loop over list of layer groups and filter out those that are animations ######            
-            for (let lg of layerGroups.layerGroups.layerGroup) {                                                
-                
-                if (lg.name.startsWith("anim_")) {
-                    result.push(lg);                
+            for (let lg of layerGroups.layerGroups.layerGroup) {
+
+                if (lg.name.startsWith(prefix)) {
+                    result.push(lg);
                 }
             }
             //###### END Loop over list of layer groups and filter out those that are animations ######
-            
+
             handler(result);
-        });                
+        });
     }
-    
-    
-    // getAnimations2 loops through all workspaces with a name that begins with "anim_" and picks
+
+
+    // getAnimations2 loops through all workspaces with a name that begins with the passed prefix and picks
     // from each of those workspaces the one group layer that has the same name as the workspace.
-    loadAnimationsFromAllWorkspaces(handler : any) {
-        
-        let me = this;
-        
-        this.loadAnimationWorkspacesAsync(function(workspaces : any) {                       
-            
-            let result : Array<any> = [];
-            
+    loadAnimationsFromAllWorkspaces(prefix: string, handler: any) {
+
+        this.loadAnimationWorkspacesAsync(prefix, (workspaces: any) => {
+
+            let result: Array<any> = [];
+
             let numAnims = workspaces.length;
             let count = 0;
-                                    
-            for(let ws of workspaces) {
-                                              
-                me.gsRestApi.loadLayerGroupAsync(ws.name, ws.name, function(lg : any) {
+
+            for (let ws of workspaces) {
+
+                this.gsRestApi.loadLayerGroupAsync(ws.name, ws.name, function (layerGroup: any) {
 
                     // TODO: 2 What if a request fails?
-                    result.push(lg);
+                    result.push(layerGroup);
                     count++;
 
                     if (count == numAnims) {
@@ -77,28 +78,28 @@ export class GeoServerAnimationApi {
                         handler(result);
                     }
                 });
-                
-            }            
+
+            }
         });
     }
-    
-    // TODO: 3 Move name filtering to GeoServerRestReader API
-    loadAnimationWorkspacesAsync(handler : any) {
-        
-        this.gsRestApi.loadWorkspacesAsync(function (workspaces : any) {
-                                         
+
+
+    loadAnimationWorkspacesAsync(prefix: string, handler: any) {
+
+        this.gsRestApi.loadWorkspacesAsync((workspaces: any) => {
+
             let result = [];
-            
+
             //###### BEGIN Loop over list of workspaces and filter out those that are animations ######            
             for (let ws of workspaces) {
-                                                
-                if (ws.name.startsWith("anim_")) {
-                    result.push(ws);                
+
+                if (ws.name.startsWith(prefix)) {
+                    result.push(ws);
                 }
             }
             //###### END Loop over list of workspaces and filter out those that are animations ######
-                                               
+
             handler(result);
-        });                
+        });
     }
 }
